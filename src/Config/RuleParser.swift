dd8921802faac19a0b7270f5463f31f90cbf349e
lei <still.magical@icloud.com>
2017-05-28 00:43:45 +0800

    Use Json install yaml

--- src/Config/RuleParser.swift
+++ src/Config/RuleParser.swift
@@ -1,8 +1,8 @@
 import Foundation
-import Yaml
+import SwiftyJSON
 
 struct RuleParser {
-    static func parseRuleManager(_ config: Yaml, adapterFactoryManager: AdapterFactoryManager) throws -> RuleManager {
+    static func parseRuleManager(_ config: JSON, adapterFactoryManager: AdapterFactoryManager) throws -> RuleManager {
         guard let ruleConfigs = config.array else {
             throw ConfigurationParserError.noRuleDefined
         }
@@ -15,7 +15,7 @@ struct RuleParser {
         return RuleManager(fromRules: rules, appendDirect: true)
     }
 
-    static func parseRule(_ config: Yaml, adapterFactoryManager: AdapterFactoryManager) throws -> Rule {
+    static func parseRule(_ config: JSON, adapterFactoryManager: AdapterFactoryManager) throws -> Rule {
         guard let type = config["type"].string?.lowercased() else {
             throw ConfigurationParserError.ruleTypeMissing
         }
@@ -36,7 +36,7 @@ struct RuleParser {
         }
     }
 
-    static func parseCountryRule(_ config: Yaml, adapterFactoryManager: AdapterFactoryManager) throws -> CountryRule {
+    static func parseCountryRule(_ config: JSON, adapterFactoryManager: AdapterFactoryManager) throws -> CountryRule {
         guard let country = config["country"].string else {
             throw ConfigurationParserError.ruleParsingError(errorInfo: "Country code (country) is required for country rule.")
         }
@@ -56,7 +56,7 @@ struct RuleParser {
         return CountryRule(countryCode: country, match: match, adapterFactory: adapter)
     }
 
-    static func parseAllRule(_ config: Yaml, adapterFactoryManager: AdapterFactoryManager) throws -> AllRule {
+    static func parseAllRule(_ config: JSON, adapterFactoryManager: AdapterFactoryManager) throws -> AllRule {
         guard let adapter_id = config["adapter"].stringOrIntString else {
             throw ConfigurationParserError.ruleParsingError(errorInfo: "An adapter id (adapter_id) is required.")
         }
@@ -68,7 +68,7 @@ struct RuleParser {
         return AllRule(adapterFactory: adapter)
     }
 
-    static func parseDomainListRule(_ config: Yaml, adapterFactoryManager: AdapterFactoryManager) throws -> DomainListRule {
+    static func parseDomainListRule(_ config: JSON, adapterFactoryManager: AdapterFactoryManager) throws -> DomainListRule {
         guard let adapter_id = config["adapter"].stringOrIntString else {
             throw ConfigurationParserError.ruleParsingError(errorInfo: "An adapter id (adapter_id) is required.")
         }
@@ -100,7 +100,7 @@ struct RuleParser {
         }
     }
 
-    static func parseIPRangeListRule(_ config: Yaml, adapterFactoryManager: AdapterFactoryManager) throws -> IPRangeListRule {
+    static func parseIPRangeListRule(_ config: JSON, adapterFactoryManager: AdapterFactoryManager) throws -> IPRangeListRule {
         guard let adapter_id = config["adapter"].stringOrIntString else {
             throw ConfigurationParserError.ruleParsingError(errorInfo: "An adapter id (adapter_id) is required.")
         }
@@ -127,7 +127,7 @@ struct RuleParser {
         }
     }
 
-    static func parseDNSFailRule(_ config: Yaml, adapterFactoryManager: AdapterFactoryManager) throws -> DNSFailRule {
+    static func parseDNSFailRule(_ config: JSON, adapterFactoryManager: AdapterFactoryManager) throws -> DNSFailRule {
         guard let adapter_id = config["adapter"].stringOrIntString else {
             throw ConfigurationParserError.ruleParsingError(errorInfo: "An adapter id (adapter_id) is required.")
         }
